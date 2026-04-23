a488c70dfbe71d573aa6cec38cab1686d5f8a0f4
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-22 19:51:05 -0500

    Various fixes

--- test/Soenneker.Utils.String.Trie.Tests/TrieSimilarityStringUtilTests.cs
+++ test/Soenneker.Utils.String.Trie.Tests/TrieSimilarityStringUtilTests.cs
@@ -11,17 +11,18 @@ public class TrieSimilarityStringUtilTests : HostedUnitTest
     {
     }
 
-    [Theory]
-    [InlineData("hello", "hell", 80.0)]
-    [InlineData("abc", "abcd", 75.0)]
-    [InlineData("abc", "xyz", 0.0)]
-    [InlineData("prefix", "pre", 50.0)]
-    [InlineData("", "", 100.0)]
-    [InlineData("abcdef", "abcxyz", 50.0)]
-    [InlineData("i bought me a cat", "i bought me a duck", 77.0)]
+    [Test]
+    [Arguments("hello", "hell", 80.0)]
+    [Arguments("abc", "abcd", 75.0)]
+    [Arguments("abc", "xyz", 0.0)]
+    [Arguments("prefix", "pre", 50.0)]
+    [Arguments("", "", 100.0)]
+    [Arguments("abcdef", "abcxyz", 50.0)]
+    [Arguments("i bought me a cat", "i bought me a duck", 77.0)]
     public void CalculateSimilarity_ShouldReturnExpectedResult(string str1, string str2, double expected)
     {
         double actual = TrieSimilarityStringUtil.CalculateSimilarityPercentage(str1, str2);
         actual.Should().BeApproximately(expected, 1);
     }
 }
+
